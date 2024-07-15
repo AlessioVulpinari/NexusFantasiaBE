6ddbf9b02c4466fadac1eb1db1fe23d3ee852f26
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-07-15 16:13:05 +0200

    Subclasses Entity set-ups
    - subclasses
    - subclasses level

---
+++ src/main/java/alessiovulpinari/NexusFantasiaBE/entities/Subclass.java
@@ -0,0 +1,42 @@
+package alessiovulpinari.NexusFantasiaBE.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "sottoclassi")
+public class Subclass {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_sottoclasse", nullable = false)
+    private UUID subclassId;
+
+    @Column(name = "nome_sottoclasse", nullable = false)
+    private String name;
+
+    @Column(name = "descrizione_sottoclasse", nullable = false)
+    private String description;
+
+    public Subclass(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.subClassLevels = new HashSet<>();
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "sottoclassi_livelli",
+            joinColumns = @JoinColumn(name = "id_sottoclasse"),
+            inverseJoinColumns = @JoinColumn(name = "id_livello"))
+    Set<SubclassLevel> subClassLevels;
+}
---
+++ src/main/java/alessiovulpinari/NexusFantasiaBE/entities/SubclassLevel.java
@@ -0,0 +1,33 @@
+package alessiovulpinari.NexusFantasiaBE.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "livelli_sottoclassi")
+public class SubclassLevel {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_livello_sottoclasse", nullable = false)
+    private UUID subclassLevelId;
+
+    @Column(name = "numero_livello")
+    private int levelNumber;
+
+    @OneToOne
+    @Column(name = "tratto_sottoclasse")
+    private ClassFeature classFeature;
+
+    public SubclassLevel(int levelNumber, ClassFeature classFeature) {
+        this.levelNumber = levelNumber;
+        this.classFeature = classFeature;
+    }
+}
